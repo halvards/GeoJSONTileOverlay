f5caf64d8a6fe5a1bc6fbdef146f389de78cc81a
Halvard Skogsrud <halvard@skogsrud.com>
2015-04-13 14:19:05 +1000

    Add support for MultiLineString GeoJSON objects
    Also removed SwiftyJSON dependency.

--- GeoJSONTileOverlay.swift
+++ GeoJSONTileOverlay.swift
@@ -12,23 +12,50 @@ class GeoJSONTileOverlay: MKTileOverlay {
         let request = NSURLRequest(URL: self.URLForTilePath(path))
         NSURLConnection.sendAsynchronousRequest(request, queue: queue,
                 completionHandler: {
-                    (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-                    if (error == nil) {
-                        let features = JSON(data: data)["features"].arrayValue
+                    (response: NSURLResponse!, data: NSData!, httpError: NSError!) -> Void in
+                    if let error = httpError {
+                        return result(data, error)
+                    }
+                    var jsonError: NSError?
+                    let jsonObject: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &jsonError)
+                    if let error = jsonError {
+                        return result(data, error)
+                    }
+                    let dataString = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    if let features = (jsonObject as? NSDictionary)?["features"] as? NSArray {
                         for feature in features {
-                            let jsonCoordinates = feature["geometry"]["coordinates"].arrayValue
-                            var coords: [CLLocationCoordinate2D] = []
-                            coords.reserveCapacity(jsonCoordinates.count)
-                            for jsonCoordinate in jsonCoordinates {
-                                let longitude: CLLocationDegrees = jsonCoordinate[0].doubleValue
-                                let latitude: CLLocationDegrees = jsonCoordinate[1].doubleValue
-                                coords.append(CLLocationCoordinate2DMake(latitude, longitude))
+                            if let geometry = (feature as? NSDictionary)?["geometry"] as? NSDictionary {
+                                self.addGeometry(geometry)
                             }
-                            let polyline = MKPolyline(coordinates: &coords, count: coords.count)
-                            self.polylines.append(polyline)
                         }
                     }
-                    result(data, error)
+                    result(data, nil)
                 })
+    }
+
+    func addGeometry(geometry: NSDictionary) {
+        if let coordinates = geometry["coordinates"] as? NSArray {
+            if let type = geometry["type"] as? NSString {
+                if type == "LineString" {
+                    self.addLineString(coordinates)
+                } else if type == "MultiLineString" {
+                    for lineString in coordinates {
+                        self.addLineString(lineString as? NSArray)
+                    }
+                }
+            }
+        }
+    }
+
+    func addLineString(geoJsonCoordinates: NSArray!) {
+        var polylineCoordinates: [CLLocationCoordinate2D] = []
+        polylineCoordinates.reserveCapacity(geoJsonCoordinates.count)
+        for geoJsonCoordinate in geoJsonCoordinates {
+            let longitude: CLLocationDegrees = geoJsonCoordinate[0].doubleValue
+            let latitude: CLLocationDegrees = geoJsonCoordinate[1].doubleValue
+            polylineCoordinates.append(CLLocationCoordinate2DMake(latitude, longitude))
+        }
+        let polyline = MKPolyline(coordinates: &polylineCoordinates, count: polylineCoordinates.count)
+        self.polylines.append(polyline)
     }
 }
--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 If you have a [TileStache](http://tilestache.org/) server providing [vector tiles](http://tilestache.org/doc/#vector-provider) in [GeoJSON](http://geojson.org/geojson-spec.html) format, these classes will allow you to render this data on iOS devices running iOS 7.0+ using MapKit.
 
+Use them just as you would the built-in MKTileOverlay and MKTileOverlayRenderer classes.
+
 ## Limitations
 
-* Assumes all geometries are [LineString](http://geojson.org/geojson-spec.html#linestring)s
-* Depends on [SwiftyJSON](https://github.com/SwiftyJSON/SwiftyJSON). Note that if you are using Swift 1.2 (XCode 6.3), you will need to use SwiftyJSON from the [xcode6.3 branch](https://github.com/SwiftyJSON/SwiftyJSON/tree/xcode6.3)
+* Only handles [LineString](http://geojson.org/geojson-spec.html#linestring)s and [MultiLineString](http://geojson.org/geojson-spec.html#multilinestring)s
 
